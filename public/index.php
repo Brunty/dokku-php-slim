3f453bdb2619242edfd2edddfd0a315bd0502e8b
Matt Brunt <matt@mfyu.co.uk>
2017-08-06 17:25:24 +0100

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello world!");
+
+    return $response;
+});
+
+$app->run();
